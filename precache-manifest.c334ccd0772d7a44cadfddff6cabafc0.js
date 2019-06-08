1c16a20680f2f250e549235cc8b41e567f3c4ce0
chiheb <chiheb.rabeh@gmail.com>
2019-06-08 21:18:49 +0100

    Updates

---
+++ precache-manifest.c334ccd0772d7a44cadfddff6cabafc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "656a84de76b6bf3482d4d5b09e06f928",
+    "url": "/setting-state/./index.html"
+  },
+  {
+    "revision": "881799ccc7f6c0f7395d",
+    "url": "/setting-state/./static/css/main.55bd0e2c.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/setting-state/./static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "881799ccc7f6c0f7395d",
+    "url": "/setting-state/./static/js/main.82e0a625.chunk.js"
+  },
+  {
+    "revision": "f83c603bcc289e8a1796",
+    "url": "/setting-state/./static/js/runtime~main.8909bbb6.js"
+  }
+]);
